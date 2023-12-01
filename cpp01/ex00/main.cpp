f148e3ff4b4255cf3364f3f153bc4f485b767d7d
InryeolChoi <inchoi@cx2r2s8.42seoul.kr>
2023-12-01 13:51:34 +0900

    cpp01 ex00, ex03 수정

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -2,20 +2,6 @@
 
 int main()
 {
-	// 스택(정적 할당)으로 만든 좀비
-	std::cout << "<< zombies made by stack >>" << std::endl;
-	Zombie zombie1("zombie1");
-	Zombie zombie2("zombie2");
-	Zombie zombie3;
-
-	zombie1.announce();
-	zombie2.announce();
-	zombie3.announce();
-
-	// random으로 만든 좀비
-	randomChump("randomChump");
-	std::cout << std::endl;
-
 	// 힙(동적 할당)으로 만든 좀비
 	std::cout << "<< zombies made by heap >>" << std::endl;
 	Zombie *zombieX;
@@ -23,6 +9,21 @@ int main()
 	zombieX = newZombie("zombieX");
 	zombieX->announce();
 	delete zombieX;
+	std::cout << std::endl;
 
+	// random으로 만든 좀비
+	std::cout << "<< zombies made by random >>" << std::endl;
+	randomChump("randomChump");
+	std::cout << std::endl;
+
+	// 스택(정적 할당)으로 만든 좀비
+	std::cout << "<< zombies made by stack >>" << std::endl;
+	Zombie zombie;
+	Zombie zombie1("zombie1");
+	Zombie zombie2("zombie2");
+
+	zombie.announce();
+	zombie1.announce();
+	zombie2.announce();
 	return (0);
 }
