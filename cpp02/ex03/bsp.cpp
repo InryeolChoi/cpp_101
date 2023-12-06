bcd86d443e3b7a134cbb2712d0299bfb65b4fd46
InryeolChoi <dlsfuf0316@gmail.com>
2023-12-06 11:24:19 +0900

    cpp02 ex03 수정

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -1,12 +1,38 @@
 #include "Point.hpp"
 
-bool	bsp(Point const a, Point const b, Point const c, Point const point)
+bool bsp(Point const a, Point const b, Point const c, Point const point)
 {
-	bool	sign[3];
+	bool ret = false;
 
-	sign[0] = ((a.getX() - point.getX()) * (b.getY() - a.getY()) - (b.getX() - a.getX()) * (a.getY() - point.getY())) > 0;
-	sign[1] = ((b.getX() - point.getX()) * (c.getY() - b.getY()) - (c.getX() - b.getX()) * (b.getY() - point.getY())) > 0;
-	sign[2] = ((c.getX() - point.getX()) * (a.getY() - c.getY()) - (a.getX() - c.getX()) * (c.getY() - point.getY())) > 0;
-
-	return sign[0] == sign[1] && sign[1] == sign[2];
+	if ((a.getY() > point.getY()) != (b.getY() > point.getY()))
+	{
+		if (((b.getX() - a.getX()) * ((point.getY() - a.getY()) / (b.getY() - a.getY())) + a.getX()) >= point.getX())
+		{
+			if (ret)
+				ret = false;
+			else
+				ret = true;
+		}
+	}
+	if ((a.getY() > point.getY()) != (c.getY() > point.getY()))
+	{
+		if (((c.getX() - a.getX()) * ((point.getY() - a.getY()) / (c.getY() - a.getY())) + a.getX()) >= point.getX())
+		{
+			if (ret)
+				ret = false;
+			else
+				ret = true;
+		}
+	}
+	if ((b.getY() > point.getY()) != (c.getY() > point.getY()))
+	{
+		if (((c.getX() - b.getX()) * ((point.getY() - b.getY()) / (c.getY() - b.getY())) + b.getX()) >= point.getX())
+		{
+			if (ret)
+				ret = false;
+			else
+				ret = true;
+		}
+	}
+	return (ret);
 }
